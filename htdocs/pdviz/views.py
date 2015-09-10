fd330ddd0ec1fdb9d6d8230cbebee1fb1f900057
Mahmood Khan <mkhan@mercycorps.org>
2015-09-10 16:21:23 -0700

    created a base template and added some static resources

---
+++ htdocs/pdviz/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+from django.contrib import messages
+
+class HomeView(TemplateView):
+    template_name='index.html'
+
+    def get(self, request, *args, **kwargs):
+        messages.success(self.request, "Hey, I'm Okay.")
+        return super(HomeView, self).get(request, *args, **kwargs)    
