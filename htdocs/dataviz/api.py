60363669ca8e3f76cf2646f823d27bc748d1f74f
Mahmood Khan <mkhan@mercycorps.org>
2015-09-22 12:50:02 -0700

    #4 forgot to comit this file

---
+++ htdocs/dataviz/api.py
@@ -0,0 +1,21 @@
+from rest_framework import viewsets
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, UpdateModelMixin
+from rest_framework.viewsets import GenericViewSet
+
+from .models import *
+from .serializers import *
+from pdviz.utils import *
+
+class DonorViewSet(viewsets.ModelViewSet):
+    serializer_class = DonorSerializer
+    
+    def get_queryset(self):
+        kwargs = prepare_related_donor_fields_to_lookup_fields(self.request.GET)
+        num_grants = self.request.GET.get('grants_count', None)
+        print(num_grants)
+        if num_grants == None:
+            return Donor.objects.filter(**kwargs).annotate(grants_count=Count('grants'))
+        else:
+            return Donor.objects.filter(**kwargs).annotate(grants_count=Count('grants')).filter(grants_count__gt=num_grants)
+
+    
