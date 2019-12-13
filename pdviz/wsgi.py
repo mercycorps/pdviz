a6047a387872294e58098140bf8a8e6785cd584f
Sanjuro Jogdeo <sjogdeo@mercycorps.org>
2019-12-13 09:45:33 -0800

    Update wsgi file.

--- pdviz/wsgi.py
+++ pdviz/wsgi.py
@@ -1,30 +1,14 @@
 """
 WSGI config for pdviz project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-# import os
-# from os.path import abspath, dirname
-# from sys import path
-#
-# SITE_ROOT = dirname(dirname(abspath(__file__)))
-# #print(SITE_ROOT)
-# path.append(SITE_ROOT)
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pdviz.settings.local")
-#
-# application = get_wsgi_application()
-
 import os
-
+from os.path import abspath, dirname
+from sys import path
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdviz.settings.local')
+SITE_ROOT = dirname(dirname(abspath(__file__)))
+path.append(SITE_ROOT)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pdviz.settings.local")
 
 application = get_wsgi_application()
