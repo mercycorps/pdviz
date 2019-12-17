5850196be511829dd560cb01195378f86f028151
Sanjuro Jogdeo <sjogdeo@mercycorps.org>
2019-12-16 17:17:22 -0800

    Use GAIT server for GAIT queries as a separate DB.  Add database router.

--- dataviz/serializers.py
+++ dataviz/serializers.py
@@ -3,7 +3,7 @@ import locale
 from rest_framework import serializers
 from .models import *
 
-locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 class GrantSerializerPlain(serializers.ModelSerializer):
     gait_id = serializers.SerializerMethodField("get_grant_id")
