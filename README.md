0343ec91cef8ec44f49e935023dbb6731586fc6b
Andre Thomas <anthomas@mercycorps.org>
2021-09-15 15:24:46 -0400

    Upgrade libraries, updated the local example file, added a saml config example file, updated the README.

--- README.md
+++ README.md
@@ -6,3 +6,88 @@ Proposal Development Visualization
 * Clone the dependent repos, [feedback](https://github.com/mercycorps/feedback) and [djangocosign](https://github.com/mercycorps/djangocosign)
 * Make sure the local.py file is up-to-date.
 * 
+
+
+
+
+
+
+
+
+
+
+# pdviz
+
+Proposal Development Visualization
+
+## SETUP INSTRUCTIONS
+
+### TESTED ENVIRONMENT
+- Python 3.6.x - 3.8.x
+- djangorestframework-3.12.4
+- Django 3.2.7
+
+
+### DEPENDENCIES
+1. Install python3-dev
+2. Install mysql
+
+
+## INSTALL DJANGO CODEBASE
+### Clone the repo and create a virtualenv for python3
+Create a directory for this project which will contain the repo and the virtualenv.
+```bash
+$ mkdir /Users/<username>/pdviz_dir
+$ cd pdviz_dir
+$ git clone https://github.com/mercycorps/pdviz.git
+$ virtualenv -p python3.8 venv
+$ source venv/bin/activate  # you should see '(venv)' appear on the left of your command prompt
+```
+
+### Activate the virtualenv and `pip-sync requirements.txt`
+Install `pip-tools` and use the `pip-sync` command to install, upgrade, and uninstall your virtual environment with the necessary dependencies to match the `requirements.txt` contents.
+```bash
+$ pip install pip-tools
+$ pip-sync requirements.txt
+```
+
+### Copy local_example.py to local.py and modify as needed.
+Modify at minimum the following fields:
+    ```yaml
+    SECRET_KEY: "your_key_here"
+    SETTINGS_DIR: "XXXXXXXXXXX"
+    DATABASES:
+        default:
+            NAME: "<db_name>" # "pdviz"
+            ENGINE: "django.db.backends.mysql"
+            HOST: "localhost"
+            USER: "<db_usernaem>" # "root"
+            PASSWORD: "<password>" # "SooperSekritWord"
+            PORT: ""
+        gait:
+            NAME: "<db_name>" # "grants"
+            ENGINE: "django.db.backends.mysql"
+            HOST: "localhost"
+            USER: "<db_usernaem>" # "root"
+            PASSWORD: "<password>" # "SooperSekritWord"
+            OPTIONS:
+                charset: "latin1"
+    ```
+
+### Copy the saml_config_example.yml file to saml_config.yml.  
+To enable the Okta login on a server, the values for each of the settings in the saml_config.yml will need to be updated with your own values and/or those from an Okta XML config file sent to you by the Okta administrator. 
+
+## Creating a local user
+You will need to create a local user so you can log in.
+You can use MySQL to create a local superuser
+
+#### Using MySQL
+Use the `createsuperuser` command and enter in a username and password to be used to login. When it asks for an email, you can just hit the Enter key to skip the question (email not needed to login).
+```bash
+$ python manage.py createsuperuser
+Username: <my_username>
+Email address:
+Password: <my_password>
+Password (again): <my_password>
+Superuser created successfully.
+```
--- pdviz/settings/local_example.py
+++ pdviz/settings/local_example.py
@@ -1,22 +1,37 @@
-from base import *
+import yaml
+from .base import *
+from os.path import dirname, join, abspath
+
+
+SETTINGS_DIR = dirname(abspath(__file__))
+
+DEBUG = True
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+SECRET_KEY = 'YOUR_SECRET_KEY_GOES_HERE...MAKE_SURE_TO_UPDATE_IT!!!'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'replace_with_your_db_name',  # Or path to database file if using sqlite3.
-        'USER': 'replace_with_your_db_user',  # Not used with sqlite3.
-        'PASSWORD': 'replace_with_your_db_password',  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'NAME': 'pdviz',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '',
+    },
+    'gait': {
+        'NAME': 'grants',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
         'OPTIONS': {
-            'sql_mode': 'traditional',
-        },      
-    }           
+            'charset': 'latin1',
+        },
+    }
 }
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'YOUR_SECRET_KEY_GOES_HERE...MAKE_SURE_TO_UPDATE_IT!!!'
-                
+DATABASE_ROUTERS = ['dataviz.models.DBRouter']
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.example.org'
 EMAIL_PORT = 587
@@ -25,3 +40,38 @@ EMAIL_HOST_PASSWORD = 'password'
 DEFAULT_FROM_EMAIL = 'no-reply@example.org'
 SERVER_EMAIL = "admin@example.org"
 DEFAULT_TO_EMAIL = SERVER_EMAIL
+
+ADMINS = (
+    ('Admins', 'admins@example.com'),
+)
+MANAGERS = ADMINS
+
+"""
+CRISPY_TEMPLATE_PACK='bootstrap3'
+
+
+### This is to map Django message levels to Boostrap3 alert levels ########
+MESSAGE_TAGS = {message.DEBUG: 'debug',
+                message.INFO: 'info',
+                message.SUCCESS: 'success',
+                message.WARNING: 'warning',
+                message.ERROR: 'danger',}
+"""
+
+
+# Social auth setup
+def read_yaml(path):
+    with open(path) as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+    return data
+
+
+saml_settings = read_yaml(join(SETTINGS_DIR, 'saml_config.yml'))
+
+SOCIAL_AUTH_SAML_SP_ENTITY_ID = saml_settings.get('SOCIAL_AUTH_SAML_SP_ENTITY_ID', None)
+SOCIAL_AUTH_SAML_SP_PUBLIC_CERT = saml_settings.get('SOCIAL_AUTH_SAML_SP_PUBLIC_CERT', None)
+SOCIAL_AUTH_SAML_SP_PRIVATE_KEY = saml_settings.get('SOCIAL_AUTH_SAML_SP_PRIVATE_KEY', None)
+SOCIAL_AUTH_SAML_ORG_INFO = saml_settings.get('SOCIAL_AUTH_SAML_ORG_INFO', None)
+SOCIAL_AUTH_SAML_TECHNICAL_CONTACT = saml_settings.get('SOCIAL_AUTH_SAML_TECHNICAL_CONTACT', None)
+SOCIAL_AUTH_SAML_SUPPORT_CONTACT = saml_settings.get('SOCIAL_AUTH_SAML_SUPPORT_CONTACT', None)
+SOCIAL_AUTH_SAML_ENABLED_IDPS = saml_settings.get('SOCIAL_AUTH_SAML_ENABLED_IDPS', None)
